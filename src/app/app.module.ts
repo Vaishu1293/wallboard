f56b2b2d8f571b447a586033d9c52933366a78d2
Vaishu1293 <clearexamseasy@gmail.com>
2023-09-10 20:11:18 +0100

    v3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CallListComponent } from './call-list/call-list.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { CallListComponent } from './call-list/call-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/call-list/call-list.component.ts
+++ src/app/call-list/call-list.component.ts
@@ -1,41 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CallListServiceService } from '../call-list-service.service';
+
 
 @Component({
   selector: 'app-call-list',
   templateUrl: './call-list.component.html',
-  styleUrls: ['./call-list.component.css']
+  styleUrls: ['./call-list.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CallListComponent implements OnInit {
 
-  callData: any = {};  // Specify the type here
+  callData: any = [];  // Specify the type here
   columns: any[] = [];
+  class_table: string = 'table-red';
 
   constructor(private callListService: CallListServiceService) { }
 
   ngOnInit(): void {
     setInterval(() => {
       this.callListService.getCallData().subscribe((data: { [key: string]: number }) => {
-        // console.log(data);
+
         if (this.columns.length < 1){
           this.columns = Object.keys(data[0]);
         }
         // console.log(this.columns);
         this.callData = data;
+        console.log(Array.isArray(this.callData));
       });
     }, 2000);  // Fetch new data every 2 seconds
   }
 
   getRowClass(longestWaitingCall: string): string {
     // Convert "Longest Waiting Call" to total minutes
+    console.log('Longest Waiting Call:', longestWaitingCall);
+
     const [minutes, seconds] = longestWaitingCall.split(':').map(Number);
     const totalMinutes = minutes + (seconds / 60);
 
     // Compare with 5 minutes
     if (totalMinutes > 5) {
-      return 'table-danger';  // Red background for more than 5 minutes
+      return 'table-red tr';  // Red background for more than 5 minutes
     } else {
-      return 'table-primary';  // Blue background for the rest
+      return 'table-green tr';  // Blue background for the rest
     }
   }
 
